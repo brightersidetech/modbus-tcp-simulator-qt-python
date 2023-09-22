3ebc1ad1cd0488dfd8aa8c392ab2c5d61f4783a1
brightersidetech <brightersidetech@gmail.com>
2023-09-22 19:20:36 +0300

    Cleaning up, added all read function codes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from typing import Optional
 from PySide6 import QtCore, QtWidgets, QtGui
-import sys, time, os
+import sys, time
 from pyModbusTCP.client import ModbusClient
+
 
 class Worker(QtCore.QObject):
     # signal
     modbus_data = QtCore.Signal(list)
-    button_state = QtCore.Signal(int)
+    modbus_status = QtCore.Signal(int)
 
     def __init__(self, parent=None):
         QtCore.QObject.__init__(self, parent=parent)
@@ -26,17 +26,18 @@ class Worker(QtCore.QObject):
         self.port = int(port)
         if not self.modbus_master:
             print("Connecting to Modbus Server...", self.host, self.port)
-            self.modbus_master = ModbusClient(host=self.host, port=self.port, unit_id=self.unit_id, 
-                                            auto_open=self.auto_open, auto_close=self.auto_close)
+            self.modbus_master = ModbusClient(host=self.host, port=self.port, unit_id=self.unit_id,
+                                              auto_open=self.auto_open, auto_close=self.auto_close)
             QtCore.QThread.sleep(2)
-            
+
             if self.modbus_master.open():
                 print("Connected to Modbus Server!")
                 self.running = True
-                self.button_state.emit(1)
+                self.modbus_status.emit(1)
             else:
                 print("Connection to Modbus Server Failed!")
                 self.modbus_master = None
+                self.modbus_status.emit(0)
 
     @QtCore.Slot()
     def disconnect(self):
@@ -45,27 +46,37 @@ class Worker(QtCore.QObject):
             self.running = False
             self.modbus_master.close()
             self.modbus_master = None
-            self.button_state.emit(2)
+            self.modbus_status.emit(2)
 
-    @QtCore.Slot(int, int)    
-    def read_registers(self, start_address, no_registers):
-        #while self.running:
+    @QtCore.Slot(int, int, str)
+    def read_registers(self, start_address, no_registers, function_code):
+        # while self.running:
         print(start_address)
         print(no_registers)
+        print(function_code)
         if self.running:
-            self.response = self.modbus_master.read_holding_registers(start_address, no_registers)
+            # Choose function code
+            if function_code == "Read Holding Registers (0x03)":
+                self.response = self.modbus_master.read_holding_registers(start_address, no_registers)
+            elif function_code == "Read Discrete Inputs (0x02)":
+                self.response = self.modbus_master.read_discrete_inputs(start_address, no_registers)
+            elif function_code == "Read Input Registers (0x04)":
+                self.response = self.modbus_master.read_input_registers(start_address, no_registers)
+
             if self.response:
                 print(self.response)
                 self.modbus_data.emit(self.response)
+                self.modbus_status.emit(5)
             else:
-                print("read error") 
+                print("read error")
+                self.modbus_status.emit(4)
             time.sleep(2)
 
 
 class ModbusMaster(QtWidgets.QMainWindow):
     stop_signal = QtCore.Signal()
     connect_signal = QtCore.Signal(int, int, str, int)
-    read_signal = QtCore.Signal(int, int)
+    read_signal = QtCore.Signal(int, int, str)
 
     def __init__(self):
         super().__init__()
@@ -89,13 +100,13 @@ class ModbusMaster(QtWidgets.QMainWindow):
         self.worker = Worker()
         self.worker.moveToThread(self.thread)
 
-        #self.thread.started.connect(self.worker.connect) # when thread starts, start worker
-        #self.thread.finished.connect(self.worker.stop) # when thread finishes, stop worker
-        #self.stop_signal.connect(self.worker.stop)
+        # self.thread.started.connect(self.worker.connect) # when thread starts, start worker
+        # self.thread.finished.connect(self.worker.stop) # when thread finishes, stop worker
+        # self.stop_signal.connect(self.worker.stop)
         self.connect_signal.connect(self.worker.modbus_connect)
         self.read_signal.connect(self.worker.read_registers)
         self.worker.modbus_data.connect(self.modbus_data_handler)
-        self.worker.button_state.connect(self.modbus_button_handler)
+        self.worker.modbus_status.connect(self.modbus_status_handler)
         self.thread.start()
 
         self.button_layout = QtWidgets.QHBoxLayout(self)
@@ -127,77 +138,35 @@ class ModbusMaster(QtWidgets.QMainWindow):
         self.modbus_request.no_of_registers.valueChanged.connect(self.draw_registers)
         self.modbus_registers = ModbusRegisters()
 
-        # registers layout
-        # self.register_layout = QtWidgets.QGridLayout()
-        # # registers group
-        # self.registers_group = QtWidgets.QGroupBox()
-        # self.registers_group.setLayout(self.register_layout)
-
-        # # generate registers
-        # self.registers = []
-        # # for i in range(10):
-        # #     regs = [] 
-        # #     for j in range(10):
-        # #         reg = QtWidgets.QLineEdit(placeholderText='-/-',)
-        # #         reg.setReadOnly(True)
-        # #         reg.setFixedWidth(50)
-        # #         reg.setFixedHeight(50)
-        # #         regs.append(reg)
-        # #     self.registers.append(regs)
-        # for i in range(1,100):
-        #         reg = QtWidgets.QLineEdit(placeholderText='-/-',)
-        #         reg.setReadOnly(True)
-        #         reg.setFixedWidth(40)
-        #         reg.setFixedHeight(40)
-        #         self.registers.append(reg)
-
-        # i = 0
-        # j = 0
-        # for reg in self.registers:
-        #     self.register_layout.addWidget(reg, i, j)
-        #     j =j + 1
-        #     if j == 9:
-        #         j = 0
-        #         i = i + 1
-            # for j in range(10):
-            #     self.register_layout.addWidget(self.registers[i], j, i%10)
-
-
-
-            # self.register_layout.addWidget(self.registers[i], j, i%10)
-            # if i%10 == 0 and i != 0:
-            #     j = j +1
-
-        
-        # for i in range(10):
-        #     for j in range(10):
-        #         self.register_layout.addWidget(self.registers[i][j], i, j)
-
-
+        # Registers table
         self.table = QtWidgets.QTableWidget(self)
-        #self.setCentralWidget(self.table)
+        # self.setCentralWidget(self.table)
 
-        self.table.setColumnCount(10)
-        self.table.setRowCount(10)
-        #self.table.setColumnWidth(50)
-        #self.table.set
+        self.table.setColumnCount(1)
+        self.table.setRowCount(1)
         self.table.horizontalHeader().hide()
         self.table.verticalHeader().hide()
-        for i in range(10):
-            self.table.setRowHeight(i, 30)
-            self.table.setColumnWidth(i, 30)
+        self.table.setRowHeight(0, 30)
+        self.table.setColumnWidth(0, 30)
+
+        # for i in range(10):
+        #     self.table.setRowHeight(i, 30)
+        #     self.table.setColumnWidth(i, 30)
         # self.table.setColumnWidth(2, 50)
 
         self.table.setItem(0, 0, QtWidgets.QTableWidgetItem('-/-'))
-                
+
+        # Modbus status message
+        self.modbus_status_message = QtWidgets.QLabel()
+        self.modbus_status_message.setHidden(True)
 
         # add widgets to main layout
-        #self.layout_main.addWidget(self.group)
+        self.layout_main.addWidget(self.modbus_status_message)
         self.layout_main.addWidget(self.slave_properties)
         self.layout_main.addWidget(self.modbus_request)
         self.layout_main.addWidget(self.table)
-        #self.layout_main.addWidget(self.modbus_registers)
-        
+        # self.layout_main.addWidget(self.modbus_registers)
+
         # add main widget to main window
         self.setCentralWidget(self.main_window)
 
@@ -224,7 +193,6 @@ class ModbusMaster(QtWidgets.QMainWindow):
         settings_action.triggered.connect(self.open_dialog)
         setting_menu.addAction(settings_action)
 
-
         # toolbar
         toolbar = QtWidgets.QToolBar('Main ToolBar')
         self.addToolBar(toolbar)
@@ -247,23 +215,19 @@ class ModbusMaster(QtWidgets.QMainWindow):
 
         # toolbar.addAction(exit_action)
 
-
         # status bar
         self.status_bar = self.statusBar()
-        
-        # display the a message in 5 seconds
-        self.status_bar.showMessage('Ready', 5000)
-
         # add a permanent widget to the status bar
         self.character_count = QtWidgets.QLabel("Length: 0")
+        self.statusbar_address_text = "TCP: " + self.address + ":" + str(self.port)
+        self.server_address_port = QtWidgets.QLabel(self.statusbar_address_text)
+        self.status_bar.addPermanentWidget(self.server_address_port)
         self.status_bar.addPermanentWidget(self.character_count)
 
-
         # settings dialog
-    
-        self.settings_dilaog = SettingsDialog()
-        self.settings_dilaog.settings_signal.connect(self.save_server_settings)
-    
+        self.settings_dialaog = SettingsDialog()
+        self.settings_dialaog.settings_signal.connect(self.save_server_settings)
+
     @QtCore.Slot(str, str)
     def save_server_settings(self, address, port):
         print(address)
@@ -271,34 +235,37 @@ class ModbusMaster(QtWidgets.QMainWindow):
         self.address = address
         self.port = int(port)
 
-
     # When stop_btn is clicked this runs. Terminates the worker and the thread.
     def stop_thread(self):
         self.worker.running = False
         self.stop_signal.emit()  # emit the finished signal on stop
 
-
     def quit(self):
-        #if self.confirm_save():
+        # if self.confirm_save():
         self.destroy(sys.exit(1))
 
     def open_dialog(self):
-        #setting_dilaog = Form()
-        self.settings_dilaog.exec()
+        # setting_dilaog = Form()
+        self.settings_dialaog.exec()
 
     QtCore.Slot()
+
     def connect_modbus(self):
         slave_id = self.slave_properties.slave_address_selector.value()
         slave_scan_rate = self.slave_properties.slave_scan_rate.value()
         self.connect_signal.emit(slave_id, slave_scan_rate, self.address, self.port)
 
     QtCore.Slot()
+
     def read_registers(self):
         start_address = self.modbus_request.start_adddress.value()
         number_of_registers = self.modbus_request.no_of_registers.value()
-        self.read_signal.emit(start_address, number_of_registers)
+        function_code = self.modbus_request.func_code_select.currentText()
+        print("Selected Function code ", function_code)
+        self.read_signal.emit(start_address, number_of_registers, function_code)
 
     QtCore.Slot(list)
+
     def modbus_data_handler(self, data):
         print(data)
         # for reg in range(0, data_length):
@@ -307,42 +274,55 @@ class ModbusMaster(QtWidgets.QMainWindow):
         j = 0
         for reg in data:
             self.table.setItem(i, j, QtWidgets.QTableWidgetItem(str(reg)))
-            j = j+1
+            j = j + 1
 
             if j > 9:
-                i = i +1
+                i = i + 1
                 j = 0
 
     QtCore.Slot(int)
+
     def draw_registers(self, registers):
         print(registers)
-        if registers%10 == 0:
-            i = registers/10
-        else:    
-            i = registers/10 + 1
+        if registers % 10 == 0:
+            i = registers / 10
+        else:
+            i = registers / 10 + 1
 
         self.table.setRowCount(i)
         self.table.setColumnCount(10)
         self.fill_reg_table()
 
     QtCore.Slot(int)
-    def modbus_button_handler(self, enum):
+
+    def modbus_status_handler(self, enum):
         if enum == 1:
+            self.modbus_status_message.setVisible(False)
             self.btn_start.setDisabled(True)
             self.btn_stop.setDisabled(False)
             self.btn_read.setDisabled(False)
         elif enum == 2:
             self.btn_start.setDisabled(False)
             self.btn_stop.setDisabled(True)
-            self.btn_read.setDisabled(True)   
+            self.btn_read.setDisabled(True)
+        elif enum == 0:
+            self.modbus_status_message.setText("Cannot connect to Modbus Server")
+            self.modbus_status_message.setStyleSheet("QLabel { background-color : red; color : white; }")
+            self.modbus_status_message.setVisible(True)
+        elif enum == 4:
+            self.modbus_status_message.setVisible(True)
+            self.modbus_status_message.setText("Error Reading Registers")
+        elif enum == 5:
+            self.modbus_status_message.setVisible(False)
 
     def fill_reg_table(self):
         rows = self.table.rowCount()
         columns = self.table.columnCount()
         for i in range(rows):
             for j in range(columns):
-                self.table.setItem(i, j, QtWidgets.QTableWidgetItem('-/-'))         
-
+                self.table.setRowHeight(i, 30)
+                self.table.setColumnWidth(j, 30)
+                self.table.setItem(i, j, QtWidgets.QTableWidgetItem('-/-'))
 
 
 class SlaveProperties(QtWidgets.QWidget):
@@ -356,12 +336,12 @@ class SlaveProperties(QtWidgets.QWidget):
         self.slave_address_label = QtWidgets.QLabel("Slave Address", alignment=QtCore.Qt.AlignLeft)
         self.slave_address_selector = QtWidgets.QSpinBox(minimum=1, maximum=100, value=1, alignment=QtCore.Qt.AlignLeft)
         self.slave_scan_rate_label = QtWidgets.QLabel("Scan Rate (ms)")
-        self.slave_scan_rate = QtWidgets.QSpinBox (minimum=1000, maximum=10000, value=1000, singleStep=1000)
+        self.slave_scan_rate = QtWidgets.QSpinBox(minimum=1000, maximum=10000, value=1000, singleStep=1000)
         self.layout.addWidget(self.slave_address_label)
         self.layout.addWidget(self.slave_address_selector)
         self.layout.addWidget(self.slave_scan_rate_label)
         self.layout.addWidget(self.slave_scan_rate)
-        #self.layout.addWidget(self.button)
+        # self.layout.addWidget(self.button)
         self.layout.addStretch()
 
         # groupbox 1
@@ -372,18 +352,22 @@ class SlaveProperties(QtWidgets.QWidget):
         self.lay = QtWidgets.QVBoxLayout(self)
         self.lay.addWidget(self.group)
 
+
 class ModbusRequest(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(ModbusRequest, self).__init__(parent)
 
         # modbus requests layout
-        #self.modbus_request = QtWidgets.QGridLayout()
+        # self.modbus_request = QtWidgets.QGridLayout()
         self.modbus_request = QtWidgets.QHBoxLayout()
 
         self.func_code_label = QtWidgets.QLabel("Function Code")
         self.func_code_select = QtWidgets.QComboBox()
+
+        self.func_code_select.addItem("Read Holding Registers (0x03)")
+        self.func_code_select.addItem("Read Discrete Inputs (0x02)")
+        self.func_code_select.addItem("Read Input Registers (0x04)")
         self.func_code_select.addItem("Read Coils (0x01)")
-        self.func_code_select.addItem("Read Discrete (0x02)")
 
         self.start_address_label = QtWidgets.QLabel("Start Address")
         self.start_adddress = QtWidgets.QSpinBox(minimum=0, maximum=100, value=0, alignment=QtCore.Qt.AlignLeft)
@@ -406,63 +390,45 @@ class ModbusRequest(QtWidgets.QWidget):
         self.lay = QtWidgets.QHBoxLayout(self)
         self.lay.addWidget(self.modbus_request_group)
 
+
 class ModbusRegisters(QtWidgets.QWidget):
     def __init__(self, parent=None):
-        super(ModbusRegisters, self).__init__(parent)        
-    # registers layout
+        super(ModbusRegisters, self).__init__(parent)
+        # registers layout
         self.register_layout = QtWidgets.QGridLayout()
         # registers group
         self.registers_group = QtWidgets.QGroupBox()
         self.registers_group.setLayout(self.register_layout)
-        self.registers_group.resize(200,50)
+        self.registers_group.resize(200, 50)
 
         # generate registers
         self.registers = []
-        # for i in range(10):
-        #     regs = [] 
-        #     for j in range(10):
-        #         reg = QtWidgets.QLineEdit(placeholderText='-/-',)
-        #         reg.setReadOnly(True)
-        #         reg.setFixedWidth(50)
-        #         reg.setFixedHeight(50)
-        #         regs.append(reg)
-        #     self.registers.append(regs)
-        for i in range(1,100):
-                reg = QtWidgets.QLineEdit(placeholderText='-/-',)
-                reg.setReadOnly(True)
-                reg.setFixedWidth(40)
-                reg.setFixedHeight(40)
-                self.registers.append(reg)
+        for i in range(1, 100):
+            reg = QtWidgets.QLineEdit(placeholderText='-/-', )
+            reg.setReadOnly(True)
+            reg.setFixedWidth(40)
+            reg.setFixedHeight(40)
+            self.registers.append(reg)
 
-        i = 0
-        j = 0
-        for reg in self.registers:
-            self.register_layout.addWidget(reg, i, j)
-            j =j + 1
-            if j == 9:
-                j = 0
-                i = i + 1
-            # for j in range(10):
-            #     self.register_layout.addWidget(self.registers[i], j, i%10)
+        self.register_layout.addWidget(self.registers[0], 0, 0)
 
+        # i = 0
+        # j = 0
 
-
-            # self.register_layout.addWidget(self.registers[i], j, i%10)
-            # if i%10 == 0 and i != 0:
-            #     j = j +1
-
-        
-        # for i in range(10):
-        #     for j in range(10):
-        #         self.register_layout.addWidget(self.registers[i][j], i, j)
+        # for reg in self.registers:
+        #     self.register_layout.addWidget(reg, i, j)
+        #     j =j + 1
+        #     if j == 9:
+        #         j = 0
+        #         i = i + 1
 
         self.lay = QtWidgets.QHBoxLayout(self)
         self.lay.addWidget(self.registers_group)
 
 
-
 class SettingsDialog(QtWidgets.QDialog):
     settings_signal = QtCore.Signal(str, str)
+
     def __init__(self):
         super().__init__()
 
@@ -477,9 +443,9 @@ class SettingsDialog(QtWidgets.QDialog):
         # Create widgets
         self.server_label = QtWidgets.QLabel("Server Address")
         self.server = QtWidgets.QLineEdit("10.6.129.81")
-        self.server.setInputMask( "000.000.000.000")
+        self.server.setInputMask("000.000.000.000")
         self.port_label = QtWidgets.QLabel("Server Port")
-        self.port = QtWidgets.QLineEdit("520")
+        self.port = QtWidgets.QLineEdit("502")
 
         # Create layout and add widgets
         layout = QtWidgets.QGridLayout()
@@ -487,8 +453,8 @@ class SettingsDialog(QtWidgets.QDialog):
         layout.addWidget(self.server, 0, 1)
         layout.addWidget(self.port_label, 1, 0)
         layout.addWidget(self.port, 1, 1)
-        
-        #layout.addWidget(self.button)
+
+        # layout.addWidget(self.button)
         layout.addWidget(self.buttonBox, 2, 0, 1, 2)
         # Set dialog layout
         self.setLayout(layout)
@@ -497,11 +463,11 @@ class SettingsDialog(QtWidgets.QDialog):
         print(self.server.text())
         print(self.port.text())
         self.settings_signal.emit(self.server.text(), self.port.text())
-        
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     modbusWidget = ModbusMaster()
     modbusWidget.show()
-    modbusWidget.resize(800,300)
+    modbusWidget.resize(800, 300)
     sys.exit(app.exec())
